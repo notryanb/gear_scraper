5cc5e173c4f24918500148e5fc2ed369a2919c8f
Ryan Blecher <notryanb@gmail.com>
2019-02-27 16:04:22 -0500

    change to unhtml for parsing and try it out

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-fn main() -> Result<(), Box<std::error::Error>> {
-    let response = reqwest::get("https://longisland.craigslist.org/search/msa")?;
 
-    println!("{:?}", response);
+use std::default::Default;
+use unhtml::{self, FromHtml};
+
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut escaped_html_text = reqwest::get("https://longisland.craigslist.org/search/msa")?.text()?;
+
+
+    #[derive(FromHtml)]
+    #[html(selector = "#test")]
+    struct Listing {
+        #[html(selector = "p:nth-child(1)", attr = "inner")]
+        name: String,
+
+        #[html(selector = "p:nth-child(2)", attr = "inner")]
+        age: u8,
+
+        #[html(selector = "p:nth-child(3)", attr = "inner")]
+        like_lemon: bool,
+    }
+
+
     Ok(())
 }
