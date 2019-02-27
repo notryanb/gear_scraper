9e60e9605945181760aaa1ea2956fd90eb2197ac
Ryan Blecher <notryanb@gmail.com>
2019-02-27 18:30:14 -0500

    play around with getting multiple nested selectors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 
-use std::default::Default;
-use unhtml::{self, FromHtml};
+#[macro_use]
+extern crate unhtml_derive;
+extern crate unhtml;
 
+use unhtml::{FromHtml, VecFromHtml};
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let mut escaped_html_text = reqwest::get("https://longisland.craigslist.org/search/msa")?.text()?;
+    // let escaped_html_text = reqwest::get("https://longisland.craigslist.org/search/msa")?.text()?;
 
 
-    #[derive(FromHtml)]
-    #[html(selector = "#test")]
-    struct Listing {
+    // #[derive(Debug, FromHtml)]
+    // struct ResultMeta {
+    //     #[html(selector = "span:nth-child(1)", attr = "inner")]
+    //     pub price: String,
+
+    //     #[html(selector = "span:nth-child(2)", attr = "inner")]
+    //     pub neighborhood: String,
+    // }
+
+    // #[derive(Debug, FromHtml)]
+    // #[html(selector = ".result-meta")]
+    // struct ResultInfo {
+    //     #[html(selector = "span")]
+    //     pub results: Vec<ResultMeta>,
+    // }
+
+    // let listings_meta = ResultInfo::from_html(&escaped_html_text)?;
+
+    // println!("{:?}", listings_meta.results);
+
+
+
+    #[derive(Debug, FromHtml)]
+    struct TestUser {
         #[html(selector = "p:nth-child(1)", attr = "inner")]
         name: String,
 
@@ -19,6 +42,62 @@ fn main() -> Result<(), Box<std::error::Error>> {
         #[html(selector = "p:nth-child(3)", attr = "inner")]
         like_lemon: bool,
     }
+
+    #[derive(Debug, FromHtml)]
+    // #[html(selector = ".test")]
+    struct TestUsers {
+        #[html(selector = ".test > span")]
+        pub users: Vec<TestUser>,
+    }
+
+    #[derive(Debug, FromHtml)]
+    // #[html(selector = "#thing")]
+    struct Everything {
+        #[html(selector = "#thing")]
+        pub users: Vec<TestUsers>,
+    }
+
+
+    let everything = Everything::from_html(r#"<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Title</title>
+        </head>
+        <body>
+            <div id=thing>
+                <div class="test">
+                    <span>
+                        <p>Hexilee</p>
+                        <p>20</p>
+                        <p>true</p>
+                    </span>
+                    <span>
+                        <p>BigBrother</p>
+                        <p>21</p>
+                        <p>false</p>
+                    </span>
+                </div>
+                <div class="test">
+                    <span>
+                        <p>Ryan</p>
+                        <p>20</p>
+                        <p>true</p>
+                    </span>
+                    <span>
+                        <p>Caryn</p>
+                        <p>21</p>
+                        <p>false</p>
+                    </span>
+                </div>
+            </div>
+        </body>
+        </html>"#).unwrap();
+
+
+
+    println!("{:?}", everything);
+
 
 
     Ok(())
