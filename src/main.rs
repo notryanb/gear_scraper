1055da7cd7fa70e1863ada3c3f756a6817e399a2
Ryan Blecher <notryanb@gmail.com>
2019-03-14 15:42:40 -0400

    Parse out datetime, pid, title, and post url from a single search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 #[macro_use]
 extern crate unhtml_derive;
 extern crate unhtml;
@@ -7,6 +6,33 @@ use unhtml::{FromHtml, VecFromHtml};
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let escaped_html_text = reqwest::get("https://longisland.craigslist.org/search/msa")?.text()?;
+
+    #[derive(Debug, FromHtml)]
+    struct SearchResults {
+        #[html(selector = "li.result-row")]
+        pub results: Vec<ResultRow>,
+    }
+
+    #[derive(Debug, FromHtml)]
+    struct ResultRow {
+        #[html(attr = "data-pid", default = 0)]
+        pub pid: i64,
+
+        #[html(selector = "a.result-image", attr = "href", default = "-")]
+        pub posting_url: String,
+
+        #[html(selector = "p.result-info", default = "-")]
+        pub result_info: ResultInfo,
+    }
+
+    #[derive(Debug, FromHtml)]
+    struct ResultInfo {
+        #[html(selector = "time.result-date", attr = "datetime", default = "--")]
+        pub date_time: String,
+
+        #[html(selector = "a.result-title", attr = "inner", default = "--")]
+        pub title: String,
+    }
 
     #[derive(Debug, FromHtml)]
     struct ResultMetas {
@@ -23,10 +49,13 @@ fn main() -> Result<(), Box<std::error::Error>> {
         pub neighborhood: String,
     }
 
-    let result_metas = ResultMetas::from_html(&escaped_html_text)?;
+    let result_metas = SearchResults::from_html(&escaped_html_text)?;
 
     for result in result_metas.results {
-        println!("Price: {}, Neighboorhood: {}", result.price, result.neighborhood);
+        println!(
+            "PID: {}, DateTime: {:?}, Title: {:?},  URL: {}",
+            result.pid, result.result_info.date_time, result.result_info.title, result.posting_url
+        );
     }
 
     Ok(())
